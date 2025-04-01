315f055dddba20cb06250d0a3ffe2a0d206c1153
AminduBhashana <waamidu@gmail.com>
2025-04-01 18:38:08 +0530

    create about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -2,13 +2,41 @@ import Image from 'next/image'
 import styles from './about.module.css'
 
 const AboutPage = () => {
-    return(
-        <div>
-            <div className={styles.imgContainer}>
-                <Image src="/about-image.png" alt='About image' fill/>    
-            </div> 
+    return (
+      <div className={styles.container}>
+        <div className={styles.textContainer}>
+          <h2 className={styles.subTitle}>About NewVehicles</h2>
+          <h1 className={styles.title}>
+            Welcome to the Ultimate Destination for New Car Arrivals in Sri
+            Lanka!
+          </h1>
+          <p className={styles.description}>
+            Step into the world of elite automotive excellence with Sri Lanka’s
+            newest luxury car arrivals. From high-performance engines to opulent
+            interiors, discover the finest models from Mercedes-Benz, BMW, Audi,
+            and more. Experience prestige on the road with exclusive showroom
+            offers and personalized financing plans.
+          </p>
+          <div className={styles.boxes}>
+            <div className={styles.box}>
+                <h1>10+</h1>
+                <p>Years of Experience</p>
+            </div>
+            <div className={styles.box}>
+                <h1>25+</h1>
+                <p>New Vehicle Arrivals</p>
+            </div>
+            <div className={styles.box}>
+                <h1>10 K+</h1>
+                <p>Daily Blog Users</p>
+            </div>
+          </div>
         </div>
-    )
+        <div className={styles.imgContainer}>
+          <Image className={styles.img} src="/about-image.png" alt="About image" fill />
+        </div>
+      </div>
+    );
 }
 
 export default AboutPage
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,15 @@
+import styles from './footer.module.css';
+
 const Footer = () => {
     return(
-        <div>Footer</div>
+        <>
+        <div className={styles.container}>
+            <div className={styles.logo}>NewVehicles</div>
+            <div className={styles.text}>
+                New Vehicles 2025 © All Rights Reserved
+            </div>
+        </div>
+        </>
     )
 }
 

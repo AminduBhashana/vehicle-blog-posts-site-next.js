1231cde03d235259afffff14979f870387012f44
AminduBhashana <waamidu@gmail.com>
2025-04-28 12:26:04 +0530

    remove github login

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,6 +1,5 @@
 
 import NextAuth from "next-auth";
-import GitHub from "next-auth/providers/github";
 import { connectToDb } from "./utils";
 import { User } from "./models";
 import CredentialsProvider from "next-auth/providers/credentials";
@@ -34,10 +33,6 @@ export const {
   signOut
 } = NextAuth({
   providers: [
-    GitHub({
-      clientId: process.env.AUTH_GITHUB_ID,
-      clientSecret: process.env.AUTH_GITHUB_SECRET,
-    }),
     CredentialsProvider({
       async authorize(credentials) {
         try {
@@ -52,26 +47,4 @@ export const {
   ],
   secret: process.env.AUTH_SECRET,
   basePath: "/api/auth",
-  callbacks: {
-    async signIn({user, account, profile}){
-        if(account.provider === "github"){
-            connectToDb();
-            try{
-                const user = await User.findOne({email: profile.email});
-
-                if(!user){
-                    const newUser = new User({
-                        username: profile.name,
-                        email: profile.email,                     
-                    });
-                    await newUser.save();
-                }
-            }catch(err){
-                console.log(err);
-                return false;
-            }
-        }
-        return true;
-    }
-  }
 });
